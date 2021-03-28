c4484ccade4cf21e936b177502e5c516a5af44b4
maryowo <mary159240@gmail.com>
2021-03-29 04:46:24 +0800

    +

--- main.js
+++ main.js
@@ -4,22 +4,26 @@ $("#banner").slick({
     autoplaySpeed: 2500
 });
 
+
+
 // 服務項目
 var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    speed: 1600,
-    pagination: '.swiper-pagination',
-    paginationBulletRender: function (swiper, index, className) {
-      var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-      return '<span class="' + className + '">' + year + '</span>';
-    },
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    breakpoints: {
-      768: {
-        direction: 'horizontal',
-      }
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
     }
-  });
+  }
+});
+
+
